446f4fcc887fac30099eb32d05b2cb7ed18c76f8
RayenBourguiba <rayen.bourguiba01@gmail.com>
2026-03-16 17:27:00 +0100

    Update views.py

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,9 +1,10 @@
-from rest_framework.decorators import api_view, permission_classes, renderer_classes
+from rest_framework.decorators import api_view, permission_classes, renderer_classes, authentication_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
 @api_view(["GET"])
+@authentication_classes([])
 @permission_classes([AllowAny])
 @renderer_classes([JSONRenderer])
 def health(request):
